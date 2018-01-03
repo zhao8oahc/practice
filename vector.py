9037aa703eca95afcc267679000d27357aee51c9
nest <zhao3oahc@gmail.com>
2018-01-03 20:56:10 +0800

    line practice

--- vector.py
+++ vector.py
@@ -25,6 +25,8 @@ class Vector(object):
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
 
+    def __getitem__(self, item):
+        return self.coordinates[item]
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
@@ -154,14 +156,14 @@ class Vector(object):
     def area_of_triangle_with(self,v):
         return self.area_of_parallelogram_with(v) / Decimal('2.0')
 
-v = Vector(['8.462','7.893','-8.187'])
-w = Vector(['6.984','-5.975','4.778'])
-print v.cross(w)
+#v = Vector(['8.462','7.893','-8.187'])
+#w = Vector(['6.984','-5.975','4.778'])
+#print v.cross(w)
 
-v=Vector(['-8.987','-9.838','5.031'])
-w=Vector(['-4.268','-1.861','-8.866'])
-print v.area_of_parallelogram_with(w)
+#v=Vector(['-8.987','-9.838','5.031'])
+#w=Vector(['-4.268','-1.861','-8.866'])
+#print v.area_of_parallelogram_with(w)
 
-v=Vector(['1.5','9.542','3.691'])
-w=Vector(['-6.007','0.124','5.772'])
-print v.area_of_triangle_with(w)
+#v=Vector(['1.5','9.542','3.691'])
+#w=Vector(['-6.007','0.124','5.772'])
+#print v.area_of_triangle_with(w)
